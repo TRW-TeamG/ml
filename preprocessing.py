e76c134ee150280cf4326ee70f600c5084e7976f
martinytso <martinytso99@gmail.com>
2025-01-16 16:05:54 -0800

    modified into a function

--- preprocessing.py
+++ preprocessing.py
@@ -1,40 +1,35 @@
 import json
 import pandas as pd
-file_path= "exampleData.json"
 
+def process_json(file_path):
+    # Open and load the JSON file
+    with open(file_path, 'r') as file:
+        data = json.load(file)
 
-# Open and load the JSON file
-with open(file_path, 'r') as file:
-    data = json.load(file)
+    df = data['holdings']
 
-df= data['holdings']
-
-tokens_df = pd.DataFrame( # dataframe that describes current wallet holdings
-    [{
-        'token': item['token'], 
-        'balance': item['balance'], 
-        'value_usd': item['value_usd']
-    } for item in df]
-)
-
-recent_trades = [] #dataframe that has recent trades
-for item in df:
-    for trade in item['recent_trades']:
-        trade_data = {
+    tokens_df = pd.DataFrame( # dataframe that describes current wallet holdings
+        [{
             'token': item['token'], 
-            'type': trade['type'], 
-            'price': trade['price'], 
-            'timestamp': trade['timestamp']
-        }
-        recent_trades.append(trade_data)
+            'balance': item['balance'], 
+            'value_usd': item['value_usd']
+        } for item in df]
+    )
 
-trades_df = pd.DataFrame(recent_trades)
+    recent_trades = [] # dataframe that has recent trades
+    for item in df:
+        for trade in item['recent_trades']:
+            trade_data = {
+                'token': item['token'], 
+                'type': trade['type'], 
+                'price': trade['price'], 
+                'timestamp': trade['timestamp']
+            }
+            recent_trades.append(trade_data)
 
+    trades_df = pd.DataFrame(recent_trades)
 
-print("Tokens Overview:")
-print(tokens_df)
+    return tokens_df,trades_df
 
-print("\nRecent Trades:")
-print(trades_df)
-
-print(df)
+# Example usage
+print(process_json("exampleData.json")[0])
